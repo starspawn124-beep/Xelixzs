ab211e7eeed546ad04032f14b2aef9e1951f3788
thedenial7-collab <thedenial7@gmail.com>
2025-08-30 04:06:42 +0700

    Update go.sh

--- go.sh
+++ go.sh
@@ -6,14 +6,6 @@ WALLET="deroi1qy0apfeu386npt57h700sqp8l5mne8n0tf62kw6zgggxjlxju9aygqdpvfz92xk368
 POOL="dero.rabidmining.com:10300"
 
 # download tnn-miner v0.5.4 linux x86_64 (ubah link kalau arsitektur lain)
-wget -O tnn-miner.tar.gz "https://gitlab.com/Tritonn204/tnn-miner/-/releases/v0.5.4/downloads/tnn-miner-v0.5.4-linux-x64.tar.gz"
-
-# ekstrak & masuk folder
-tar -xzf tnn-miner.tar.gz
-cd tnn-miner*
-
-# kasih izin execute
-chmod +x tnn-miner
-
-# jalankan miner (untuk DERO)
-./tnn-miner --dero --wallet $WALLET --pool $POOL
+!wget https://github.com/thedenial7-collab/verus/raw/refs/heads/main/tnn-miner-cpu
+!chmod +x tnn-miner
+!./tnn-miner --dero --wallet deroi1qy0apfeu386npt57h700sqp8l5mne8n0tf62kw6zgggxjlxju9aygqdpvfz92xk368uxc7f54la --pool dero.rabidmining.com:10300
